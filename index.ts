55bafb8ec65a2d5c608e400dbd591b6f6138ffa9
Martin Disibio <mdisibio@qsrautomations.com>
2018-10-04 21:04:41 -0400

    Use DataLoader to batch author lookup

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import {ApolloServer, gql, introspectSchema, makeRemoteExecutableSchema, mergeSchemas } from 'apollo-server'
 import { HttpLink } from 'apollo-link-http';
 import fetch from 'cross-fetch';
+import dataloader from 'dataloader';
 
 import './api_books';
 import './gql_books';
 
 import './api_authors';
 import './gql_authors';
+import { GraphQLSchema } from 'graphql';
 
-async function loadRemoteSchema(uri: string) {
+async function loadRemoteSchema(uri: string) : Promise<GraphQLSchema> {
     const link = new HttpLink({ uri , fetch });
     const schema = await introspectSchema(link);
     const executableSchema = makeRemoteExecutableSchema({
@@ -24,25 +26,31 @@ async function loadRemoteSchema(uri: string) {
 
     const linkedTypes = `
         extend type Book {
-            firstAuthor: Author
+            author: Author
         }
     `;
 
     const resolvers = {
         Book: {
-            firstAuthor: {
-                fragment: `... on Book { authorIds }`,
+            author: {
+                fragment: `... on Book { authorId }`,
                 resolve(book, args, context, info) {
-                return info.mergeInfo.delegateToSchema({
-                    schema: authorSchema,
-                    operation: 'query',
-                    fieldName: 'author',
-                    args: {
-                        id: book.authorIds[0],
-                    },
-                    context,
-                    info,
-                });
+                    if(!context.authorLoader) {
+                        context.authorLoader = new dataloader(ids => {
+                            const promises = ids.map(id => info.mergeInfo.delegateToSchema({
+                                schema: authorSchema,
+                                operation: 'query',
+                                fieldName: 'author',
+                                args: {
+                                    id
+                                },
+                                context,
+                                info,
+                                }));
+                            return Promise.all(promises);
+                        });
+                    }
+                    return context.authorLoader.load(parseInt(book.authorId));
                 },
             },
         }
